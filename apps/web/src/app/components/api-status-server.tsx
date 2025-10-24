38b83fb0ce0ae00d6794a6b0f4b8e6b01552618d
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2025-10-24 20:31:32 +0700

    fix(workspace): add db package to /apps/web module

--- apps/web/src/app/components/api-status-server.tsx
+++ apps/web/src/app/components/api-status-server.tsx
@@ -1,16 +1,18 @@
+import { client } from "@/utils/orpc";
 import { db } from "@desa/db";
+import { asset } from "@desa/db/schema/asset";
 
 const ApiStatusServer = async () => {
-  const res = await db.execute("SELECT 1");
+  const res = await client.healthCheck();
+  const assets = await db.select().from(asset);
 
   return (
     <>
       <section className="rounded-lg border p-4">
         <h2 className="mb-2 font-medium">API Status (Server-Side)</h2>
         <p className="text-sm">
-          {res.rows.length > 0
-            ? "✅ API is up and running!"
-            : "❌ API is not responding."}
+          {res ? "✅ API is up and running!" : "❌ API is not responding."}
+          {assets[0].name}
         </p>
       </section>
     </>
