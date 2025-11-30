279f543dc242b53f8e21bdb504420bdaa5b2514a
sultandevin <sultan.suyudi@gmail.com>
2025-11-30 19:07:45 +0700

    fix(db): throw error if env is undefind

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,15 +1,21 @@
 import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-dotenv.config({
-  path: "../../apps/web/.env",
-});
+if (!process.env.DATABASE_URL) {
+  dotenv.config({
+    path: "../../apps/web/.env",
+  });
+}
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in environment variables.");
+}
 
 export default defineConfig({
   schema: "./src/schema",
   out: "./src/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "",
+    url: process.env.DATABASE_URL,
   },
 });
